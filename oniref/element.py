0880dd34e4ced565de3c5ea08d1272c722b59255
Tim Prince <tim.prince@gmail.com>
2021-08-28 14:46:23 -0700

    Add method to parse Klei element definitions.

--- oniref/element.py
+++ oniref/element.py
@@ -1,27 +1,46 @@
 from __future__ import annotations
+from dataclasses import dataclass
 from typing import Tuple, Optional
 
 from oniref.units import Q
 
 
-Transition = Tuple[float, 'Element']
+Transition = Tuple[float, str]
 
 
+@dataclass
 class Element:
-    def __init__(self, name: str,
-                 specific_heat_capacity: Q,
-                 thermal_conductivity: Q,
-                 density: Q,
-                 mass_per_tile: Q,
-                 low_transition: Optional[Transition] = None,
-                 high_transition: Optional[Transition] = None):
-        self.name = name
-        self.specific_heat_capacity = specific_heat_capacity
-        self.thermal_conductivity = thermal_conductivity
-        self.density = density
-        self.mass_per_tile = mass_per_tile
-        self.low_transition = low_transition
-        self.high_transition = high_transition
+    name: str
+    specific_heat_capacity: Q
+    thermal_conductivity: Q
+    density: Q
+    mass_per_tile: Q
+    low_transition: Optional[Transition] = None
+    high_transition: Optional[Transition] = None
+
+    @staticmethod
+    def from_klei(klei_dict: dict):
+        return Element(
+            name=klei_dict['elementId'],
+            specific_heat_capacity=Q(
+                klei_dict['specificHeatCapacity'], 'DTU/g/°C'
+            ),
+            thermal_conductivity=Q(
+                klei_dict['thermalConductivity'], 'DTU/(m s)/°C'
+            ),
+            density=Q(
+                klei_dict['molarMass'], 'g/mol'
+            ),
+            mass_per_tile=Q(
+                klei_dict['defaultMass'], 'g'
+            ),
+            low_transition=(
+                klei_dict['lowTemp'], klei_dict['lowTempTransitionTarget']
+            ) if 'lowTemp' in klei_dict else None,
+            high_transition=(
+                klei_dict['highTemp'], klei_dict['highTempTransitionTarget']
+            ) if 'highTemp' in klei_dict else None
+        )
 
     @property
     def thermal_diffusivity(self):
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,3 +1,51 @@
+from oniref import Element
+
+
 def test_diffusivity(water):
     assert water.thermal_diffusivity == (water.thermal_conductivity
                                          / water.specific_heat_capacity)
+
+
+def test_from_klei_simple(water):
+    assert water == Element.from_klei({
+        "elementId": "Water",
+        "maxMass": 1000,
+        "liquidCompression": 1.01,
+        "speed": 125,
+        "minHorizontalFlow": 0.01,
+        "minVerticalFlow": 0.01,
+        "specificHeatCapacity": 4.179,
+        "thermalConductivity": 0.609,
+        "solidSurfaceAreaMultiplier": 1,
+        "liquidSurfaceAreaMultiplier": 25,
+        "gasSurfaceAreaMultiplier": 1,
+        "convertId": "DirtyWater",
+        "defaultTemperature": 300,
+        "defaultMass": 1000,
+        "molarMass": 18.01528,
+        "toxicity": 0,
+        "lightAbsorptionFactor": 0.25,
+        "radiationAbsorptionFactor": 0.8,
+        "radiationPer1000Mass": 0,
+        "tags": ["AnyWater"],
+        "isDisabled": False,
+        "state": "Liquid",
+        "localizationID": "STRINGS.ELEMENTS.WATER.NAME",
+        "dlcId": ""
+    })
+
+
+def test_from_klei_transitions():
+    result = Element.from_klei({
+        "elementId": "Water",
+        "specificHeatCapacity": 1,
+        "thermalConductivity": 1,
+        "defaultMass": 1000,
+        "molarMass": 18.01528,
+        "lowTemp": 0,
+        "highTemp": 100,
+        "lowTempTransitionTarget": "Ice",
+        "highTempTransitionTarget": "Steam"
+    })
+    assert result.low_transition == (0, "Ice")
+    assert result.high_transition == (100, "Steam")
