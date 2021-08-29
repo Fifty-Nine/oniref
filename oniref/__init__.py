56757a937801c4318b77df360d2af0919abc2fac
Tim Prince <tim.prince@gmail.com>
2021-08-29 12:44:36 -0700

    Expose some additional members.

--- oniref/__init__.py
+++ oniref/__init__.py
@@ -1,3 +1,11 @@
-from oniref.elements import Element, Transition, load_klei_definitions
+from oniref.units import Q as Quantity
+from oniref.elements import (Element,
+                             State,
+                             Transition,
+                             load_klei_definitions)
 
-__all__ = ['Element', 'Transition', 'load_klei_definitions']
+__all__ = ['Element', 
+           'Quantity',
+           'State',
+           'Transition',
+           'load_klei_definitions']
