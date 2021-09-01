79cf1e9ff84f5c86cf06d1be1f6b0b266a7f8eb0
Tim Prince <tim.prince@gmail.com>
2021-09-01 15:28:55 -0700

    Add some built-in predicates.

--- oniref/predicates.py
+++ oniref/predicates.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import Any, Callable
 
-from oniref.elements import Element
+from oniref.elements import Element, State
+from oniref.units import Q
 
 SimplePredicate = Callable[[Element], bool]
 SimpleAttribute = Callable[[Element], Any]
@@ -50,6 +51,19 @@ class Attribute:
     def Is(self, v: Any) -> Predicate:
         return Predicate(lambda e: self(e) is v)
 
+    def In(self, *v: Any) -> Predicate:
+        def result(e: Element):
+            attr = self(e)
+            if len(v) == 1:
+                try:
+                    return attr in v[0]
+                except TypeError:
+                    pass
+
+            return attr in v
+
+        return Predicate(result)
+
     def __getattr__(self, name) -> Attribute:
         def attr(e: Element) -> Any:
             return getattr(self(e), name)
@@ -67,3 +81,40 @@ def Or(l: Predicate, r: Predicate) -> Predicate:
 
 def Not(p: Predicate) -> Predicate:
     return ~p
+
+
+def state() -> Attribute:
+    return Attribute(lambda e: e.state)
+
+
+def is_solid():
+    return state() == State.Solid
+
+
+def is_liquid() -> Predicate:
+    return state() == State.Liquid
+
+
+def is_gas() -> Predicate:
+    return state() == State.Gas
+
+
+def low_temp() -> Attribute:
+    return Attribute(
+        lambda e: e.low_transition and e.low_transition.temperature
+    )
+
+
+def high_temp() -> Attribute:
+    return Attribute(
+        lambda e: e.high_transition and e.high_transition.temperature
+    )
+
+
+def stable_at(temp: Q):
+    return ((low_temp().Is(None) | (low_temp() < temp))
+            & (high_temp().Is(None) | (high_temp() > temp)))
+
+
+def stable_over(tempLo: Q, tempHi: Q):
+    return stable_at(tempLo) & stable_at(tempHi)
--- tests/test_predicates.py
+++ tests/test_predicates.py
@@ -1,5 +1,11 @@
+import pytest
+
+from oniref import State, Quantity
 from oniref.predicates import (Predicate, Attribute,
-                               And, Or, Not)
+                               And, Or, Not,
+                               state, is_solid, is_liquid, is_gas,
+                               low_temp, high_temp,
+                               stable_at, stable_over)
 
 
 def test_simple_predicate(water):
@@ -110,6 +116,20 @@ def test_attr_ge(water):
     assert pred(water)
 
 
+def test_attr_in(water):
+    pred = Attribute(lambda e: 0).In([0, 1, 2])
+    assert pred(water)
+
+    pred = Attribute(lambda e: 0).In(0, 1, 2)
+    assert pred(water)
+
+    pred = Attribute(lambda e: 0).In(0)
+    assert pred(water)
+
+    pred = Attribute(lambda e: 1).In(0)
+    assert not pred(water)
+
+
 def test_attr_chain(water_elements):
     attr = Attribute(lambda e: e.low_transition)
 
@@ -122,3 +142,99 @@ def test_attr_is(water_elements):
             .target.Is(water_elements['Ice']))
     assert pred(water_elements['Water'])
     assert not pred(water_elements['Steam'])
+
+
+def test_state(water_elements):
+    pred = state() == State.Liquid
+    assert pred(water_elements['Water'])
+    assert not pred(water_elements['Ice'])
+
+    pred = state().In([State.Solid, State.Liquid])
+    assert pred(water_elements['Water'])
+    assert not pred(water_elements['Steam'])
+
+
+def test_liquid(water_elements):
+    pred = is_liquid()
+    assert pred(water_elements['Water'])
+    assert not pred(water_elements['Ice'])
+    assert not pred(water_elements['Steam'])
+
+
+def test_solid(water_elements):
+    pred = is_solid()
+    assert not pred(water_elements['Water'])
+    assert pred(water_elements['Ice'])
+    assert not pred(water_elements['Steam'])
+
+
+def test_gas(water_elements):
+    pred = is_gas()
+    assert not pred(water_elements['Water'])
+    assert not pred(water_elements['Ice'])
+    assert pred(water_elements['Steam'])
+
+
+def test_low_temp(water_elements):
+    pred = low_temp() > Quantity(10, '°C')
+    assert not pred(water_elements['Water'])
+    assert pred(water_elements['Steam'])
+
+    with pytest.raises(ValueError):
+        pred(water_elements['Ice'])
+
+
+def test_high_temp(water_elements):
+    pred = high_temp() > Quantity(10, '°C')
+    assert pred(water_elements['Water'])
+    assert not pred(water_elements['Ice'])
+
+    with pytest.raises(ValueError):
+        pred(water_elements['Steam'])
+
+
+def test_stable_at(water_elements):
+    pred110 = stable_at(Quantity(110, '°C'))
+    pred50 = stable_at(Quantity(50, '°C'))
+    predn10 = stable_at(Quantity(-10, '°C'))
+
+    assert not pred110(water_elements['Water'])
+    assert pred110(water_elements['Steam'])
+    assert not pred110(water_elements['Ice'])
+
+    assert pred50(water_elements['Water'])
+    assert not pred50(water_elements['Steam'])
+    assert not pred50(water_elements['Ice'])
+
+    assert not predn10(water_elements['Water'])
+    assert not predn10(water_elements['Steam'])
+    assert predn10(water_elements['Ice'])
+
+
+def test_stable_over(water_elements):
+    icy = stable_over(Quantity(-273, '°C'), Quantity(-10, '°C'))
+    watery = stable_over(Quantity(10, '°C'), Quantity(90, '°C'))
+    steamy = stable_over(Quantity(110, '°C'), Quantity(1000, '°C'))
+
+    assert icy(water_elements['Ice'])
+    assert not icy(water_elements['Water'])
+    assert not icy(water_elements['Steam'])
+
+    assert not watery(water_elements['Ice'])
+    assert watery(water_elements['Water'])
+    assert not watery(water_elements['Steam'])
+
+    assert not steamy(water_elements['Ice'])
+    assert not steamy(water_elements['Water'])
+    assert steamy(water_elements['Steam'])
+
+    melt = stable_over(Quantity(-10, '°C'), Quantity(10, '°C'))
+    boil = stable_over(Quantity(90, '°C'), Quantity(110, '°C'))
+
+    assert not melt(water_elements['Ice'])
+    assert not melt(water_elements['Water'])
+    assert not melt(water_elements['Steam'])
+
+    assert not boil(water_elements['Ice'])
+    assert not boil(water_elements['Water'])
+    assert not boil(water_elements['Steam'])
